0642794733b06f7990bafa6a1390c9d47e3e2f17
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-07-28 13:01:11 -0300

    Card Show Limit for Performance

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,6 @@
 import { Pensamento } from "./pensamento";
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -11,15 +11,21 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API);
+  listar(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set("_page", pagina)
+      .set("_limit", itensPorPagina);
+
+    return this.http.get<Pensamento[]>(this.API, { params });
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento);
   }
 
-  editar(pensamento:Pensamento): Observable<Pensamento> {
+  editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`;
     return this.http.put<Pensamento>(url, pensamento);
   }
@@ -29,7 +35,7 @@ export class PensamentoService {
     return this.http.delete<Pensamento>(url);
   }
 
-  buscarPorId(id:number): Observable<Pensamento> {
+  buscarPorId(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`;
     return this.http.get<Pensamento>(url);
   }
